abc83c231ce09e5f6b88477877293d3d90e74b18
Bryant James <bryant@bryantjames.com>
2026-03-31 16:41:28 -0700

    Minor: Handled undefined env

--- apps/collectors/observations-from-local-addons/src/cli.ts
+++ apps/collectors/observations-from-local-addons/src/cli.ts
@@ -5,13 +5,13 @@ import { logger } from '@eso-market-tracker/logging'
 import { constants, db, naming } from '@eso-market-tracker/database'
 
 for (const [repoPath, platform] of [
-  [process.env.TSC_XBOX_ADDON_PATH!, constants.XBOX_NA],
-  [process.env.TSC_XBOXEU_ADDON_PATH!, constants.XBOX_EU],
-  [process.env.TSC_PSNA_ADDON_PATH!, constants.PS_NA],
-  [process.env.TSC_PSEU_ADDON_PATH!, constants.PS_EU],
+  [process.env.TSC_XBOX_ADDON_PATH, constants.XBOX_NA],
+  [process.env.TSC_XBOXEU_ADDON_PATH, constants.XBOX_EU],
+  [process.env.TSC_PSNA_ADDON_PATH, constants.PS_NA],
+  [process.env.TSC_PSEU_ADDON_PATH, constants.PS_EU],
 ]) {
   const commits = git
-    .getHistoricalContentForRepo(repoPath)
+    .getHistoricalContentForRepo(repoPath!)
     .filter((i) => i.files.length)
   const observations = await git.getObservationsFromCommit(commits.at(-1)!)
   await Promise.all(
@@ -20,7 +20,7 @@ for (const [repoPath, platform] of [
         const targetPath = naming.getObservationPath(
           i.item,
           i.stats.date,
-          platform
+          platform!
         )
         logger.info(`Logging ${i.item.id} for ${i.stats.date}`)
         await db.writeToFile(i.stats, targetPath)
