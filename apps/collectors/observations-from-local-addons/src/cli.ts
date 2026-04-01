78ce86782eb271517ed766a72cfa7ba04d76a167
Bryant James <bryant@bryantjames.com>
2026-03-31 17:47:56 -0700

    Disable obsolete tests

--- apps/collectors/observations-from-local-addons/src/cli.ts
+++ apps/collectors/observations-from-local-addons/src/cli.ts
@@ -1,36 +1,3 @@
 #!/usr/bin/env -S tsx --env-file=../.env
 import 'dotenv/config'
-import * as git from './git'
-import { logger } from '@eso-market-tracker/logging'
-import { constants, db, naming } from '@eso-market-tracker/database'
-
-for (const [repoPath, platform] of [
-  [process.env.TSC_XBOX_ADDON_PATH, constants.XBOX_NA],
-  [process.env.TSC_XBOXEU_ADDON_PATH, constants.XBOX_EU],
-  [process.env.TSC_PSNA_ADDON_PATH, constants.PS_NA],
-  [process.env.TSC_PSEU_ADDON_PATH, constants.PS_EU],
-]) {
-  const commits = git
-    .getHistoricalContentForRepo(repoPath!)
-    .filter((i) => i.files.length)
-
-  const targetCommit = commits.at(-1)
-  if (!targetCommit || !platform) {
-    throw new Error('Either commit or platform null!')
-  }
-
-  const observations = await git.getObservationsFromCommit(targetCommit)
-  await Promise.all(
-    observations.map((i) => {
-      db.throttleFileWrites(async () => {
-        const targetPath = naming.getObservationPath(
-          i.item,
-          i.stats.date,
-          platform
-        )
-        logger.info(`Logging ${i.item.id} for ${i.stats.date}`)
-        await db.writeToFile(i.stats, targetPath)
-      })
-    })
-  )
-}
+throw new Error('This data has already been ingested!')
--- apps/collectors/observations-from-local-addons/src/index.test.ts
+++ apps/collectors/observations-from-local-addons/src/index.test.ts
@@ -2,18 +2,22 @@ import 'dotenv/config'
 import { describe, expect, it } from 'vitest'
 import * as git from './git'
 
-describe('observations from local addons', { timeout: 120_000 * 30 }, () => {
-  const commits = git.getHistoricalContentForRepo(
-    process.env.TSC_XBOX_ADDON_PATH!
-  )
+describe.skipIf(process.env.CI)(
+  'observations from local addons',
+  { timeout: 120_000 * 30 },
+  () => {
+    const commits = git.getHistoricalContentForRepo(
+      process.env.TSC_XBOX_ADDON_PATH!
+    )
 
-  it('can pull commit history', () => {
-    expect(commits.length).toEqual(130)
-    expect(commits.map((i) => i.files.length)).not.toContain(0)
-  })
+    it('can pull commit history', () => {
+      expect(commits.length).toEqual(130)
+      expect(commits.map((i) => i.files.length)).not.toContain(0)
+    })
 
-  it('can pull item data', async () => {
-    const observations = await git.getObservationsFromCommit(commits.at(-1)!)
-    expect(observations.length).toEqual(20265)
-  })
-})
+    it('can pull item data', async () => {
+      const observations = await git.getObservationsFromCommit(commits.at(-1)!)
+      expect(observations.length).toEqual(20265)
+    })
+  }
+)
