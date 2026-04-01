2c6c7af7d35c06214a27425dde5b3d10224c6edf
Bryant James <bryant@bryantjames.com>
2026-03-31 17:20:50 -0700

    (╯°□°）╯︵ ┻━┻

--- apps/collectors/observations-from-local-addons/src/git.ts
+++ apps/collectors/observations-from-local-addons/src/git.ts
@@ -82,7 +82,7 @@ export const getHistoricalContentForRepo = (repoPath: string): Commit[] => {
   const commits = execFileSync('git', ['rev-list', '--all'], {
     cwd: repoPath,
     encoding: 'utf8',
-    maxBuffer: 10 * 1024 * 1024,
+    maxBuffer: 1000 * 1024 * 1024,
   })
     .toString()
     .trim()
@@ -95,7 +95,7 @@ export const getHistoricalContentForRepo = (repoPath: string): Commit[] => {
       {
         cwd: repoPath,
         encoding: 'utf8',
-        maxBuffer: 10 * 1024 * 1024,
+        maxBuffer: 1000 * 1024 * 1024,
       }
     ).toString()
 
