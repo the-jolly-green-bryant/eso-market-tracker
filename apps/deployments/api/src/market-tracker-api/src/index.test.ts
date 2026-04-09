952cfc263db47edead60d98007ce6188a5482daf
Bryant James <bryant@bryantjames.com>
2026-04-09 11:32:38 -0700

    Added testing to api worker code

--- apps/deployments/api/src/market-tracker-api/src/index.test.ts
+++ apps/deployments/api/src/market-tracker-api/src/index.test.ts
@@ -1,7 +1,34 @@
-import { describe, it, expect } from 'vitest'
-import { __dedupe, __normalize } from './index'
+import { describe, it, expect, vi } from 'vitest'
+import { __dedupe, __normalize, item, Env } from './index'
 
 describe('api', async () => {
+  const env = {
+    ESO_MARKET_TRACKER: {
+      get: vi.fn(async (key: string) => {
+        if (key === '1393740546') {
+          return {
+            item: {
+              internalId: 1393740546,
+              name: 'Dreugh Wax',
+            },
+            pricing: {
+              average: 123,
+            },
+          }
+        }
+        return null
+      }),
+    },
+  }
+
+  it('fetches data', async () => {
+    const r = await item('1393740546', env as unknown as Env)
+    const body = (r && (await r.json())) as {
+      results: { pricing: { average: number } }[]
+    }
+    expect(body?.results?.at(0)?.pricing.average).toEqual(123)
+  })
+
   it('dedupes and normalizes', async () => {
     expect(
       __dedupe([{ a: 1 }, { a: 2 }, { a: 3 }, { a: 3 }], (i) =>
