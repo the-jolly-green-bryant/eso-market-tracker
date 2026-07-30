2f248df238f42ab0b0f3ed1bf718026338d3d577
Bryant James <137834469+the-jolly-green-bryant@users.noreply.github.com>
2026-07-29 20:34:52 -0700

    Allow full observation fixture test to finish

--- apps/collectors/observations-from-tsc-web-app/src/index.test.ts
+++ apps/collectors/observations-from-tsc-web-app/src/index.test.ts
@@ -7,5 +7,5 @@ describe('observations-from-tsc-web-app', async () => {
   vi.spyOn(index, 'getAppData').mockResolvedValue(sampleResults)
   it('has no errors', async () => {
     await expect(collectObservations()).resolves.not.toThrow()
-  })
+  }, 30_000)
 })
